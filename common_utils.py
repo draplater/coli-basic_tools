816a2a1a8e11c17cd9f40415aae61bff70859356
Chen Yufei <tsjzadmin@gmail.com>
2017-06-04 18:57:33 +0800

    parse dict.

--- common_utils.py
+++ common_utils.py
@@ -4,3 +4,18 @@ def set_proc_name(newname):
     buff = create_string_buffer(len(newname)+1)
     buff.value = newname
     libc.prctl(15, byref(buff), 0, 0, 0)
+
+
+def parse_dict(parser, dic):
+    option_cmd = []
+    for k, v in dic.items():
+        assert isinstance(k, str)
+        if v is True:
+            option_cmd.append("--" + k)
+        elif v is False:
+            continue
+        else:
+            option_cmd.append("--" + k)
+            option_cmd.append(str(v))
+
+    return parser.parse_args(option_cmd)
