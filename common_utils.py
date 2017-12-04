aa5b8afc6a55e98956ba309b0c83eacc469ef883
Chen Yufei <yufei.chen@pku.edu.cn>
2017-12-04 11:15:27 +0800

    attr dict

--- common_utils.py
+++ common_utils.py
@@ -140,3 +140,32 @@ def smart_open(filename, mode="r", *args, **kwargs):
     finally:
         if fh is not sys.stdout and fh is not sys.stdin:
             fh.close()
+
+
+class AttrDict(dict):
+    """A dict whose items can also be accessed as member variables.
+
+    >>> d = AttrDict(a=1, b=2)
+    >>> d['c'] = 3
+    >>> print d.a, d.b, d.c
+    1 2 3
+    >>> d.b = 10
+    >>> print d['b']
+    10
+
+    # but be careful, it's easy to hide methods
+    >>> print d.get('c')
+    3
+    >>> d['get'] = 4
+    >>> print d.get('a')
+    Traceback (most recent call last):
+    TypeError: 'int' object is not callable
+    """
+    def __init__(self, *args, **kwargs):
+        dict.__init__(self, *args, **kwargs)
+
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
