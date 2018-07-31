84c35f61a93676f9b93ff4fd4fa64662522e5ad0
Chen Yufei <yufei.chen@pku.edu.cn>
2018-07-31 13:29:30 +0800

    new style logger

--- logger.py
+++ logger.py
@@ -2,17 +2,42 @@ import logging
 
 import sys
 
+
 logFormatter = logging.Formatter(
     "%(asctime)s [%(levelname)-5.5s] [%(name)s:%(filename)s:%(lineno)s - %(funcName)s] %(message)s")
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
-console_handler = logging.StreamHandler(sys.stdout)
-console_handler.setFormatter(logFormatter)
-logger.addHandler(console_handler)
+
+def add_log_file(target_logger, path):
+    file_handler = logging.FileHandler(path)
+    file_handler.setFormatter(logFormatter)
+    target_logger.addHandler(file_handler)
+
+
+# new style: logger object
+def get_logger(*, files=(), log_to_console=True,
+               name=None,
+               level=logging.INFO):
+    this_logger = logging.Logger("logger")
+    this_logger.setLevel(level)
+    if log_to_console:
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(logFormatter)
+        this_logger.addHandler(console_handler)
+    if isinstance(files, str):
+        add_log_file(this_logger, files)
+    else:
+        assert isinstance(files, (list, tuple))
+        for path in files:
+            add_log_file(this_logger, path)
+    if name is not None:
+        this_logger.name = name
+    return this_logger
+
+
+# old style: global logger
+
+logger = get_logger()
 
 
 def log_to_file(path):
-    file_handler = logging.FileHandler(path)
-    file_handler.setFormatter(logFormatter)
-    logger.addHandler(file_handler)
+    add_log_file(logger, path)
