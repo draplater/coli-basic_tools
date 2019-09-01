d1e8444b537739ef67117dd363e8186fdb33444c
Chen Yufei <yufei.chen@pku.edu.cn>
2019-09-01 03:52:24 +0000

    lazy import flask

--- base_service.py
+++ base_service.py
@@ -1,12 +1,17 @@
-from flask import Flask
-from jsonrpc.backend.flask import JSONRPCAPI
+try:
+    from flask import Flask
+    from jsonrpc.backend.flask import JSONRPCAPI
 
 
-class WebAPIService(Flask):
-    def __init__(self):
-        super(WebAPIService, self
-              ).__init__(self.__class__.__name__,
-                         static_folder="./static/")
-        self.api = JSONRPCAPI()
-        self.add_url_rule("/api", view_func=self.api.as_view(), methods=["POST"])
-
+    class WebAPIService(Flask):
+        def __init__(self, name=None):
+            if name is None:
+                name = self.__class__.__name__
+            super(WebAPIService, self
+                  ).__init__(name, static_folder="./static/")
+            self.api = JSONRPCAPI()
+            self.add_url_rule("/api", view_func=self.api.as_view(), methods=["POST"])
+except ImportError:
+    class WebAPIService(object):
+        def __init__(self, *args, **kwargs):
+            raise Exception("requires flask and jsonrpc")
